f9ebaabab6615d0308dda62f101394c8b2647784
Bholanath <bholanathbarik9748@gmail.com>
2024-03-27 20:39:28 +0530

    Initial Commit

---
+++ Convert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Convert
+//
+//  Created by Bholanath Barik on 27/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
